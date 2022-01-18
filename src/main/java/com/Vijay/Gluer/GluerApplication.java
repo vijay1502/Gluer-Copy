09653ac45f48a40cddccad3f7260b65551e394fc
vijay1502 <pruthvikrishna97@gmail.com>
2022-01-18 13:40:46 +0530

    First Setup

---
+++ src/main/java/com/Vijay/Gluer/GluerApplication.java
@@ -0,0 +1,13 @@
+package com.Vijay.Gluer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GluerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GluerApplication.class, args);
+	}
+
+}
